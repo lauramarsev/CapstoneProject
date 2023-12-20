2e6269140349eed197fcc313fb5bd72306e1f162
lauramarsev <111280178+lauramarsev@users.noreply.github.com>
2023-12-20 18:19:25 +0100

    Add files via upload

---
+++ src/components/OrderOnline/OrderOnline.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './OrderOnline.css'
+import { images } from '../../constants';
+
+function OrderOnline() {
+  return (
+    <div className='app__order-online'>
+        <h1>We're working on it.</h1>
+    </div>
+  )
+}
+
+export default OrderOnline;
---
+++ src/constants/orderOnline.js
@@ -0,0 +1,11 @@
+import images from "./images";
+  
+  const OrderOnline = {
+        title: `WE'RE WORKING ON IT`,
+        location: "",
+        description: "Lorem ipsum dolor sit amet consectetur adipiscing elit rhoncus massa, scelerisque facilisi hac class quam ad mus conubia mi tempor.",
+        btnname: "Order",
+        image: images.underConstruction
+    }
+
+export default OrderOnline;
